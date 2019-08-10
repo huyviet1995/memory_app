9f3a996a68724834a026e278a71d3531cd5909ce
vdang95 <vdang@gustavus.edu>
2019-08-10 13:18:48 +0700

    Add and remove chosen square coordinate

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,5 +1,8 @@
 Game = function() {};
 
+/** Remove the splice  */
+Game.flippedSquareCoordinates = [] 
+
 Game.sleep = function(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -7,29 +10,38 @@ Game.sleep = function(ms) {
 Game.flipSquareAtReload = async function(coordinates) {
   for (let it = 0; it < coordinates.length; it++) {
     let coordinate = coordinates[it];
-    $(`.front-square[coordinate='${coordinate[0]},${coordinate[1]}']`).find('.flip-square-inner').addClass('flip-square-at-reload');
+    $(`.front-square .flip-square-inner[coordinate='[${coordinate[0]},${coordinate[1]}]']`).addClass('flip-square-at-reload');
   }
   await Game.sleep(1000);
   for (let it = 0; it < coordinates.length; it++) {
     let coordinate = coordinates[it];
-    $(`.front-square[coordinate='${coordinate[0]},${coordinate[1]}']`).find('.flip-square-inner').removeClass('flip-square-at-reload');
+    $(`.front-square .flip-square-inner[coordinate='[${coordinate[0]},${coordinate[1]}]']`).removeClass('flip-square-at-reload');
   }
 }
-
 
 $(document).ready(function() {
   /* Get all flip square coordinates at reload */
   let reload_square_coordinates = JSON.parse($('input[name=random-square-coordinates]').val()); 
-
   Game.flipSquareAtReload(reload_square_coordinates);
 
   /** Each square when clicked should should be flipped */
+  /** Push or remove element accordingly */ 
   $('.front-square .flip-square-inner').on('click', function() {
     if ($(this).hasClass('flip-on-click')) {
       $(this).removeClass('flip-on-click');
+      let pickedCoordinate = $(this).attr('coordinate');
+      /** remove the square coordinate */
+      for (var it = 0; it < Game.flippedSquareCoordinates.length; it++) {
+        if (Game.flippedSquareCoordinates[it] == pickedCoordinate) {
+          Game.flippedSquareCoordinates.splice(it,1);
+        }
+      }
     }
     else {
+      /** Add the square coordinate */
       $(this).addClass('flip-on-click');
+      let pickedCoordinate = $(this).attr('coordinate');
+      Game.flippedSquareCoordinates.push(pickedCoordinate);
     }
   })
 
