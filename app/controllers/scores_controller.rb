0a58bba1ef01d535b5600743821f08c219df1809
vdang95 <vdang@gustavus.edu>
2019-08-17 12:29:42 +0700

    Update data size

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -10,11 +10,11 @@ class ScoresController < ApplicationController
   private
 
   def get_score 
-    @score ||= score_params[:score] 
+    @score ||= score_params[:score].nil? ? 0 : score_params[:score]
   end
 
   def get_level 
-    @level ||= score_params[:level]
+    @level ||= score_params[:level].nil? ? 1 : score_params[:level]
   end
 
   def get_scores_arr
@@ -23,30 +23,20 @@ class ScoresController < ApplicationController
 
   def calculate_score_percentages
     scores_arr = get_scores_arr 
-    score_counts_arr = Array.new(10) {0}
-    scores_arr.each do |score| 
-      if score.between?(0,99)
-        score_counts_arr[0]+=1
-      elsif score.between?(100,199)
-        score_counts_arr[1]+=1
-      elsif score.between?(200,299)
-        score_counts_arr[2]+=1
-      elsif score.between?(300,399)
-        score_counts_arr[3]+=1
-      elsif score.between?(400,499)
-        score_counts_arr[4]+=1
-      elsif score.between?(500,599)
-        score_counts_arr[5]+=1
-      elsif score.between?(600,699)
-        score_counts_arr[6]+=1
-      elsif score.between?(700,799)
-        score_counts_arr[7]+=1
-      elsif score.between?(800,899)
-        score_counts_arr[8]+=1
-      else score.between?(900,1000)
-        score_counts_arr[9]+=1
+    array_size = (scores_arr.last - scores_arr.first) / 100 + 1;
+    score_counts_arr = Array.new(array_size) {0}
+    count = 0
+
+    scores_arr.each do |score|
+      loop do
+        if (score.between?(count*100, count*100 + 99))
+          score_counts_arr[count]+=1
+          break;
+        else
+          count+=1 
+        end
       end
-    end 
+    end
 
     # Calculate sum
     sum = 0
@@ -54,10 +44,10 @@ class ScoresController < ApplicationController
       sum = sum + score_count 
     end
 
-    @score_percentage = Array.new(10) {0}
+    @score_percentage = Array.new(array_size) {0}
     total_sum = 0 
     score_counts_arr.each_with_index do |score_count, idx|
-      if idx == 9
+      if idx == array_size - 1
         @score_percentage[9] = 100 - total_sum 
       else
         @score_percentage[idx] = (score_count*100)/sum
