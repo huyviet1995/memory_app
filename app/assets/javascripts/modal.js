908c6fc6267279d2529627ca82e67880f00e3ae5
vdang95 <vdang@gustavus.edu>
2019-08-22 10:02:46 +0700

    Can switch view between sign in and sign up

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,1 +1,31 @@
 var modal = function() {}
+
+modal.switchViews = function({option =  '#sign-in-view'} = {}) {
+  var view, otherView;
+  if (option == '#sign-in-option') {
+    view = '#sign-in-view';
+    otherView = '#sign-up-view';
+  }
+  else if (option = '#sign-up-option') {
+    view = '#sign-up-view';
+    otherView = '#sign-in-view';
+  }
+  
+  $(view).css({
+    'display': 'table-cell'
+  })
+
+  $(otherView).css({
+    'display': 'none'
+  })
+}
+
+$(document).ready(function() {
+  $('#box-header #sign-in-option').on('click', function() {
+    modal.switchViews({option: '#sign-in-option'})
+  })
+
+  $('#box-header #sign-up-option').on('click', function() {
+    modal.switchViews({option: '#sign-up-option'}) 
+  })
+})
