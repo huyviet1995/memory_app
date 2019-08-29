65d92a68389d68ea3bfe5a47f0df1d28808af78e
vdang95 <vdang@gustavus.edu>
2019-08-30 00:40:19 +0700

    update score logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -95,7 +95,11 @@ class HomeController < ApplicationController
       when 8
         return [6,6,7]
       when 9
-        return [6,6,8]
+        return [6,6,9]
+      when 10
+        return [6,6,11]
+      when 11
+        return [6,6,13]
       else
         return [6,6,15]
       end
