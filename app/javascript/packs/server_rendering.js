f0daa04e285f9ad3ae3e6d8bda15107b0ee24523
vdang95 <vdang@gustavus.edu>
2019-09-07 14:30:42 +0700

    Remove debugger from server_rendering

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,7 +1,5 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-debugger;
 var componentRequireContext = require.context("components", true);
-debugger;
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
